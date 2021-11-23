3ff5c084fd066b5dbaccb66b30ed5062c16061c1
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-23 17:37:01 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser = "Hung Le"
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`
